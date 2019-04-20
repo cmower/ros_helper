f0cce58711a31e5c0df448e54826493e3e766e72
Chris Mower <cmower@users.noreply.github.com>
2019-04-20 18:08:52 +0100

    [msgs_api] bug fix in std.py

--- src/ros_helper/msgs_api/std.py
+++ src/ros_helper/msgs_api/std.py
@@ -3,65 +3,65 @@ from std_msgs.msg import *
 class BoolMsg(Bool):
 
   def __init__(self, d):
-    super(BoolMSg, self).__init__()
+    super(BoolMsg, self).__init__()
     self.data = d
 
 class Float32Msg(Float32):
 
   def __init__(self, d):
-    super(Float32MSg, self).__init__()
+    super(Float32Msg, self).__init__()
     self.data = d
 
 class Float64Msg(Float64):
 
   def __init__(self, d):
-    super(Float64MSg, self).__init__()
+    super(Float64Msg, self).__init__()
     self.data = d
 
 class Int8Msg(Int8):
 
   def __init__(self, d):
-    super(Int8MSg, self).__init__()
+    super(Int8Msg, self).__init__()
     self.data = d
 
 class Int16Msg(Int16):
 
   def __init__(self, d):
-    super(Int16MSg, self).__init__()
+    super(Int16sSg, self).__init__()
     self.data = d
 
 class Int32Msg(Int32):
 
   def __init__(self, d):
-    super(Int32MSg, self).__init__()
+    super(Int32Msg, self).__init__()
     self.data = d
 
 class Int64Msg(Int64):
 
   def __init__(self, d):
-    super(Int64MSg, self).__init__()
+    super(Int64Msg, self).__init__()
     self.data = d
 
 class UInt8Msg(UInt8):
 
   def __init__(self, d):
-    super(UInt8MSg, self).__init__()
+    super(UInt8Msg, self).__init__()
     self.data = d
 
 class UInt16Msg(UInt16):
 
   def __init__(self, d):
-    super(UInt16MSg, self).__init__()
+    super(UInt16Msg, self).__init__()
     self.data = d
 
 class UInt32Msg(UInt32):
 
   def __init__(self, d):
-    super(UInt32MSg, self).__init__()
+    super(UInt32Msg, self).__init__()
     self.data = d
 
 class UInt64Msg(UInt64):
 
   def __init__(self, d):
-    super(UInt64MSg, self).__init__()
+    super(UInt64Msg, self).__init__()
     self.data = d
