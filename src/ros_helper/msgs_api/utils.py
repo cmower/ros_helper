25c4ef333a0ac24e1c449c965546e35f96cb60ee
Chris Mower <chris.mower@ed.ac.uk>
2019-04-30 18:39:45 +0100

    [msgs_api] added some documentation to utils.py

--- src/ros_helper/msgs_api/utils.py
+++ src/ros_helper/msgs_api/utils.py
@@ -7,7 +7,9 @@ XYZ = ['x', 'y', 'z']
 XYZW = ['x', 'y', 'z', 'w']
 
 def _mgetter(_class, _ids):
+    """Multiple gettattr."""
     return [getattr(_class, _id) for _id in _ids]
 
 def _msetter(_class, _ids, _vals):
+    """Multiple setattr."""
     for _id, _val in zip(_ids, _vals): setattr(_class, _id, _val)
