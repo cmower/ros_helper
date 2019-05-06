438f1cd2886bbdfcf7adc454494379f8e2ff7b0e
Chris Mower <chris.mower@ed.ac.uk>
2019-05-06 01:28:11 +0100

    Started new feature, simple publishers/subscribers. Needs completing.

--- src/ros_helper/__init__.py
+++ src/ros_helper/__init__.py
@@ -1,3 +1,4 @@
 import msgs_api
 import tf2_api
 import pathtools
+import simple_sub_pub
---
+++ src/ros_helper/simple_pub_sub.py
@@ -0,0 +1,31 @@
+
+"""
+See the following on how to place constraints on user inheriting these classes.
+
+https://www.youtube.com/watch?v=cKPlPJyQrt4&t=5426s
+
+i.e. in SimplePublisher, we want to check before running that generate_message function is implemented.
+
+"""
+
+class SimplePublisher(object):
+
+    def __init__(self, rospy, topic_name, topic_type, queue_size=1):
+        self.pub = rospy.Publisher(topic_name, topic_type, queue_size=queue_size)
+
+    def update(self, event):
+        self.pub.publish(self.generate_message())
+
+class SimpleSubscriber(object):
+
+    def __init__(self, rospy, topic_name, topic_type):
+        self.sub = rospy.Subscriber(topic_name, topic_type, self.callback)
+        self.received_msg = False
+        self.msg = None
+
+    def get_msg(self):
+        return self.msg
+
+    def callback(self, msg):
+        self.msg = msg
+        self.received_msg = True
