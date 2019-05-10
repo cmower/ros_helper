25a1d2450bf5f82a76ebcd1868faf9217c4b0bdc
Chris Mower <chris.mower@ed.ac.uk>
2019-05-10 17:10:12 +0100

    [msgs_api] updated interfaces and internal processing of args

--- src/ros_helper/msgs_api/visualization.py
+++ src/ros_helper/msgs_api/visualization.py
@@ -7,14 +7,30 @@ __all__= ['MarkerMsg', 'StlMeshMsg', 'CubeMsg', 'SphereMsg', 'CylinderMsg', 'Mar
 
 class MarkerMsg(Marker):
 
-    def __init__(self, marker_type, time, frame_id):
+    def __init__(self, marker_type, time=None, frame_id=None, color=None, position=None, orientation=None, scale=None):
+
+        # Required init
         super(MarkerMsg, self).__init__()
-        self.header.stamp = time
-        self.header.frame_id = frame_id
-        self.id = 0
         self.type = marker_type
-        self.action = Marker.ADD
-        self.pose.orientation.w = 1
+
+        # Handle defaults
+        if time is not None: self.add_time(time)
+        if frame_id is not None: self.add_frame_id(frame_id)
+        if color is not None: self.add_color(color)
+        if position is not None: self.add_position(position)
+        if orientation is not None:
+            self.add_orientation(orientation)
+        else:
+            # Orientation orientation is now same as base 
+            # frame, user can always update using add_orientation
+            self.pose.orientation.w = 1
+
+        # Add other params that are sometimes forgotten, user can always update later
+        self.add_id(0)
+        self.add_action(Marker.ADD)
+
+    def add_frame_id(self, frame_id):
+        self.frame_id = frame_id
 
     def add_id(self, i):
         self.id = i
@@ -45,7 +61,8 @@ class MarkerMsg(Marker):
         self.mesh_resource = filename
 
     def add_color(self, c):
-        if type(c) is list or type(c) is tuple:
+        typec = type(c)
+        if typec is list or typec is tuple:
             if len(c) == 3:
                 _msetter(self.color, RGB, c)
                 self.add_alpha(1.0)
@@ -60,37 +77,91 @@ class MarkerMsg(Marker):
 
 class StlMeshMsg(MarkerMsg):
 
-    def __init__(self, filename, time, frame_id, scale, color):
-        super(StlMeshMsg, self).__init__(Marker.MESH_RESOURCE, time, frame_id)
+    def __init__(self, time=None, frame_id=None, color=None, position=None, orientation=None, scale=None, filename=None):
+        super(StlMeshMsg, self).__init__(Marker.MESH_RESOURCE, time, frame_id, color, position, orientation, scale)
+        if filename is not None: self.add_stl_mesh(filename)
+
+    def add_stl_mesh(self, filename):
         self.add_mesh(filename, False)
-        self.add_scale(scale)
-        self.add_color(color)
         
 class SphereMsg(MarkerMsg):
     
-    def __init__(self, time, radius, frame_id, color, pos=None):
-        super(SphereMsg, self).__init__(Marker.SPHERE, time, frame_id)
-        self.add_scale([2.0 * radius]*3) 
-        self.add_color(color)
-        if pos is not None: self.add_position(pos)
+    def __init__(self, time=None, frame_id=None, color=None, position=None, scale = None, radius=None, diameter=None):
+        super(SphereMsg, self).__init__(Marker.SPHERE, time, frame_id, color, position, None, scale)
+        if radius is not None: self.add_radius(radius)
+        if diameter is not None: self.add_diameter(diameter)
 
+    def add_radius(self, r):
+        self.add_diameter(2.0*r)
+
+    def add_diameter(self, d):
+        self.add_scale([d]*3)
+
+class ArrowMsg(MarkerMsg):
+    
+    def __init__(self, time=None, frame_id=None, color=None, scale=None, start_pt=None, end_pt=None, shaft_radius=None, head_radius=None, shaft_diameter=None, head_diameter=None, head_length=None):
+        super(ArrowMsg, self).__init__(Marker.ARROW, time, frame_id, color, None, None, scale)
+        self.points = [None, None]
+        if start_pt is not None: self.add_start_pt(start_pt)
+        if end_pt is not None: self.add_end_pt(end_pt)
+        if shaft_radius is not None: self.add_shaft_diameter(2.0 * shaft_radius)
+        if head_radius is not None: self.add_head_diameter(2.0 * head_diameter)
+        if shaft_diameter is not None: self.add_shaft_diameter(shaft_diameter)
+        if head_diameter is not None: self.add_head_diameter(head_diameter)
+        if head_length is not None: self.add_head_length(head_length)
+    
+    def add_start_pt(self, p):
+        self.points[0] = p
+
+    def add_end_pt(self, p):
+        self.points[1] = p
+
+    def add_start_and_end_points(self, ps):
+        self.add_start_pt(ps[0])
+        self.add_end_pt(ps[1])
+
+    def add_shaft_diameter(self, d):
+        self.scale.x = d
+
+    def add_head_diameter(self, d):
+        self.scale.y = d
+
+    def add_head_length(self, l):
+        self.scale.z = l
+        
 class CubeMsg(MarkerMsg):
 
-    def __init__(self, time, length, width, height, frame_id, color, pos=None, ori=None):
-        super(CubeMsg, self).__init__(Marker.CUBE, time, frame_id)
-        self.add_scale([length, width, height])
-        self.add_color(color)
-        if pos is not None: self.add_position(pos)
-        if ori is not None: self.add_orientation(ori)
+    def __init__(self, time=None, frame_id=None, color=None, position=None, orientation=None, scale=None, length=None, width=None, height=None):
+        super(CubeMsg, self).__init__(Marker.CUBE, time, frame_id, color, position, orientation, scale)
+        if length is not None: self.add_length(length)
+        if width is not None: self.add_width(width)
+        if height is not None: self.add_height(height)
+
+    def add_length(self, l):
+        self.scale.x = l
+
+    def add_width(self, w):
+        self.scale.y = w
+
+    def add_height(self, h):
+        self.scale.z = h
 
 class CylinderMsg(MarkerMsg):
 
-    def __init__(self, time, radius, height, frame_id, color, pos=None, ori=None):
-        super(CylinderMsg, self).__init__(Marker.CYLINDER, time, frame_id)
-        self.add_scale([2.0*radius, 2.0*radius, height])
-        self.add_color(color)
-        if pos is not None: self.add_position(pos)
-        if ori is not None: self.add_orientation(ori)
+    def __init__(self, time=None, frame_id=None, color=None, position=None, orientation=None, scale=None, radius=None, diameter=None, height=None)
+        super(CylinderMsg, self).__init__(Marker.CYLINDER, time, frame_id, color, position, orientation, scale)
+        if radius is not None: self.add_radius(radius)
+        if diameter is not None: self.add_diameter(diameter)
+        if height is not None: self.add_height(height)
+
+    def add_radius(r):
+        self.add_diameter(2.0*r)
+
+    def add_diameter(d):
+        self.scale.x = self.scale.y = d
+
+    def add_height(h):
+        self.scale.z = h
 
 class LineStripMsg(MarkerMsg):
 
@@ -120,11 +191,18 @@ class SphereListMsg(MarkerMsg):
             
 class MarkerArrayMsg(MarkerArray):
 
-    def __init__(self, ms=None):
+    def __init__(self, markers=None, nmarkers=None):
         super(MarkerArrayMsg, self).__init__()
         if type(ms) is list:
-            self.markers = ms
+            self.markers = markers
             self.resolve_ids()
+        if nmarkers is not None: self.markers = [None]*nmarkers
+
+    def __setitem__(self, i, m):
+        self.markers[i] = m
+
+    def __getitem__(self, i):
+        return self.markers[i]
             
     def append(self, m):
         self.markers.append(m)
@@ -133,7 +211,7 @@ class MarkerArrayMsg(MarkerArray):
         for i, m in enumerate(self.markers): m.id = i
 
     @property
-    def num_markers(self):
+    def nmarkers(self):
         return len(self.markers)
 
     def add_time(self, i, t):
