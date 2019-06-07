494bd70765e7dbef39156f443f5107cc1f34a64c
Chris Mower <chris.mower@ed.ac.uk>
2019-06-07 19:06:08 +0100

    [simple_pub_sub] Added SimplePublisherSubscriber, made init_ methods rather than using __init__.

--- src/ros_helper/simple_pub_sub.py
+++ src/ros_helper/simple_pub_sub.py
@@ -1,33 +1,44 @@
 
 """
+
+Consider the following..
+
 See the following on how to place constraints on user inheriting these classes.
 
 https://www.youtube.com/watch?v=cKPlPJyQrt4&t=5426s
 
 i.e. in SimplePublisher, we want to check before running that generate_message function is implemented.
 
-Also, is it worth making a init_publisher function instead of assuming user will know to call super(UserDefPub, self).__init__(...)? Can we constrain user to this?
-
 """
 
 class SimplePublisher(object):
 
-    def __init__(self, rospy, topic_name, topic_type, queue_size=1):
+    def init_pub(self, rospy, topic_name, topic_type, hz, queue_size):
+        assert hasattr(self, 'generate_message'), "Derived class for SimplePublisher must have a generate_message method implemented."
         self.pub = rospy.Publisher(topic_name, topic_type, queue_size=queue_size)
+        rospy.Timer(rospy.Duration(1.0/float(hz)), self.update)
 
     def update(self, event):
         self.pub.publish(self.generate_message())
 
 class SimpleSubscriber(object):
 
-    def __init__(self, rospy, topic_name, topic_type):
-        self.sub = rospy.Subscriber(topic_name, topic_type, self.callback)
+    def init_sub(self, rospy, topic_name, topic_type):
+        rospy.Subscriber(topic_name, topic_type, self.callback)
         self.received_msg = False
         self.msg = None
 
-    def get_msg(self):
+    def get_message(self):
         return self.msg
 
     def callback(self, msg):
         self.msg = msg
         self.received_msg = True
+
+class SimplePublisherSubscriber(SimplePublisher, SimpleSubscriber):
+
+    def init_pub_sub(self, rospy, pub_topic_name, pub_topic_type, pub_hz, sub_topic_name, sub_topic_type, pub_queue_size=1):
+        self.init_pub(rospy, pub_topic_name, pub_topic_type, pub_hz, pub_queue_size)
+        self.init_sub(rospy, sub_topic_name, sub_topic_type)
+
+                                   
