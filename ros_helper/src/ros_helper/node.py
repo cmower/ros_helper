7f491e7608f635863d9e5f7d58ba7a5990fe828a
Chris Mower <chris.mower@ed.ac.uk>
2021-04-19 14:01:16 +0100

    Re-organize

--- ros_helper/src/ros_helper/node.py
+++ ros_helper/src/ros_helper/node.py
@@ -7,6 +7,11 @@ from sensor_msgs.msg import Joy, JointState
 
 class RosNode:
 
+    # ----------------------------------------------------------------------------------
+    #
+    # Initialization
+    # ----------------------------------------------------------------------------------
+
     def __init__(self, rospy):
         """Initialization. Note, child-classes need to make a call to super().__init__(rospy)."""
         self.__rospy = rospy
@@ -28,11 +33,21 @@ class RosNode:
         """Specify the shutdown method as baseShutdown."""
         self.__rospy.on_shutdown(self.baseShutdown)
 
+    # ----------------------------------------------------------------------------------
+    #
+    # Handling parameters
+    # ----------------------------------------------------------------------------------
+
     def getParams(self, params):
         """Gets parameters, params must be a list of tuples. Each tuple must have length 1 or 2. The first element is required, it must be the parameter name. The second element is optional, if set it will be the default value."""
         for args in params:
             name = args[0]
             self.params[name] = self.__rospy.get_param(*args)
+
+    # ----------------------------------------------------------------------------------
+    #
+    # Interface to tf2
+    # ----------------------------------------------------------------------------------
 
     def getTf(self, base_frame_id, child_frame_id):
         """Returns a transform from tf2 as a geometry_msgs/TransformStamped."""
@@ -42,6 +57,17 @@ class RosNode:
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             self.__rospy.logwarn(f'Did not recover frame {child_frame_id} in the frame {base_frame_id}!')
         return tf
+
+    def setTf(self, base_frame_id, child_frame_id, position, quaternion=[0, 0, 0, 1]):
+        """Sets a transform using tf2."""
+        self.__tf_broadcaster.sendTransform(
+            self.packTransformStampedMsg(base_frame_id, child_frame_id, position, quaternion)
+        )
+
+    # ----------------------------------------------------------------------------------
+    #
+    # Common transform conversion methods.
+    # ----------------------------------------------------------------------------------
 
     def positionFromTf2Msg(self, tf):
         """Extract position from geomety_msg/TransformStamed message."""
@@ -69,6 +95,16 @@ class RosNode:
         T[:3,-1] = self.positionFromTf2Msg(tf)
         return T
 
+    # ----------------------------------------------------------------------------------
+    #
+    # ROS message packing methods
+    # ----------------------------------------------------------------------------------
+
+    def addTimeStampToMsg(self, msg):
+        """Add time stamp to a ROS message."""
+        msg.header.stamp = self.__rospy.Time.now()
+        return msg
+
     def packTransformStampedMsg(self, base_frame_id, child_frame_id, position, quaternion=[0, 0, 0, 1]):
         """Pack a transform stamped message"""
         tf = self.addTimeStampToMsg(TransformStamped())
@@ -80,79 +116,87 @@ class RosNode:
         tf.transform.rotation.w = quaternion[3]
         return tf
 
-    def setTf(self, base_frame_id, child_frame_id, position, quaternion=[0, 0, 0, 1]):
-        """Sets a transform using tf2."""
-        self.__tf_broadcaster.sendTransform(
-            self.packTransformStampedMsg(base_frame_id, child_frame_id, position, quaternion)
-        )
+    def packPointMsg(self, position):
+        return Point(x=position[0], y=position[1], z=position[2])
+
+    # ----------------------------------------------------------------------------------
+    #
+    # Setup publishers
+    # ----------------------------------------------------------------------------------
 
     def setupPublisher(self, name, topic, msg_type, queue_size=10):
         """Setup a publisher."""
         self.pubs[name] = self.__rospy.Publisher(topic, msg_type, queue_size=queue_size)
 
-    def addTimeStampToMsg(self, msg):
-        """Add time stamp to a ROS message."""
-        msg.header.stamp = self.__rospy.Time.now()
-        return msg
-
     def setupJointStatePublisher(self, name, topic, queue_size=10):
         """Setup joint state message publisher."""
         self.setupPublisher(name, topic, JointState, queue_size=queue_size)
-
-    def publishJointState(self, name, joint_names=[], joint_positions=[], joint_velocity=[], joint_effort=[]):
-        """Publish a joint state message."""
-        msg = self.addTimeStampToMsg(JointState())
-        msg.name = joint_names
-        msg.position = joint_positions
-        msg.velocity = joint_velocity
-        msg.effort = joint_effort
-        self.pubs[name].publish(msg)
-
-    def packPointMsg(self, position):
-        msg = Point()
-        for idx, dim in enumerate('xyz'):
-            setattr(msg, dim, position[idx])
-        return msg
 
     def setupPointPublisher(self, name, topic, queue_size=10):
         """Setup a point message publisher."""
         self.setupPublisher(name, topic, Point, queue_size=queue_size)
 
-    def publishPoint(self, name, position):
-        """Publish a point message."""
-        self.pubs[name].publish(self.packPointMsg(position))
-
     def setupPointStampedPublisher(self, name, topic, queue_size=10):
         """Setup a point stamped message publisher."""
         self.setupPublisher(name, topic, PointStamped, queue_size=queue_size)
-
-    def publishPointStamped(self, name, position):
-        """Publish a point stamped message."""
-        self.pubs[name].publish(self.addTimeStampToMsg(PointStamped(point=self.packPointMsg(position))))
 
     def setupFloat64MultiArrayPublisher(self, name, topic, queue_size):
         """Setup a float64 multi array message publisher."""
         self.setupPublisher(name, topic, Float64MultiArray, queue_size=queue_size)
 
-    def publishFloat64MultiArray(self, name, data):
-        """Publish a float64 multi array message."""
-        self.pubs[name].publish(Float64MultiArray(data=data))
-
     def setupExperimentMarkerPublisher(self, name, topic, queue_size=10):
         """When running experiments, it is useful to make markers to determine when certain events occurred."""
         self.pubs[name] = self.__rospy.Publisher(topic, Int64, queue_size=queue_size)
 
-    def sendExperimentMarker(self, name, flag=0):
+    # ----------------------------------------------------------------------------------
+    #
+    # Easy-publish methods
+    # ----------------------------------------------------------------------------------
+
+    def publishJointState(self, name, joint_names=[], joint_positions=[], joint_velocity=[], joint_effort=[]):
+        """Publish a joint state message."""
+        self.pubs[name].publish(self.addTimeStampToMsg(JointState(name=joint_names, position=joint_positions, velocity=joint_velocity, effort=joint_effort)))
+
+    def publishPoint(self, name, position):
+        """Publish a point message."""
+        self.pubs[name].publish(self.packPointMsg(position))
+
+
+    def publishPointStamped(self, name, position):
+        """Publish a point stamped message."""
+        self.pubs[name].publish(self.addTimeStampToMsg(PointStamped(point=self.packPointMsg(position))))
+
+    def publishFloat64MultiArray(self, name, data):
+        """Publish a float64 multi array message."""
+        self.pubs[name].publish(Float64MultiArray(data=data))
+
+
+    def publishExperimentMarker(self, name, flag=0):
         """Publish marker, user can make what they want of the flags."""
         self.pubs[name].publish(Int64(data=flag))
+
+    # ----------------------------------------------------------------------------------
+    #
+    # Setup services
+    # ----------------------------------------------------------------------------------
 
     def startService(self, name, type, handle):
         """Start a service."""
         self.srvs[name] = self.__rospy.Service(name, type, handle)
 
+    # ----------------------------------------------------------------------------------
+    #
+    # Setup timers
+    # ----------------------------------------------------------------------------------
+
     def startTimer(self, name, frequency, handle):
         """Start a timer."""
         self.timers[name] = self.__rospy.Timer(self.__rospy.Duration(1.0/float(frequency)), handle)
+
+    # ----------------------------------------------------------------------------------
+    #
+    # Setup subscribers
+    # ----------------------------------------------------------------------------------
 
     def startSubscriber(self, name, topic, type, wait=False):
         """Start a subscriber, optionally pause and wait for the first message."""
@@ -160,10 +204,6 @@ class RosNode:
             msg = self.__rospy.wait_for_message(topic, type)
             self.__callback(msg, name)
         self.subs[name] = self.__rospy.Subscriber(topic, type, self.__callback, callback_args=name)
-
-    def __callback(self, msg, name):
-        """Internal callback method."""
-        self.msgs[name] = msg
 
     def startJoySubscriber(self, name, topic, joystick_cls, wait=False):
         """Starts a joystick subscriber that automatically parses sensor_msgs/Joy messages to joystick class from a class in joy.py script."""
@@ -173,10 +213,24 @@ class RosNode:
             self.__joy_callback(msg, args)
         self.subs[name] = self.__rospy.Subscriber(topic, Joy, self.__joy_callback, callback_args=args)
 
+    # ----------------------------------------------------------------------------------
+    #
+    # Internal callback methods
+    # ----------------------------------------------------------------------------------
+
+    def __callback(self, msg, name):
+        """Internal callback method."""
+        self.msgs[name] = msg
+
     def __joy_callback(self, msg, args):
         """Converts joy message to given joystick class and logs to the msgs class attribute."""
         name, joystick_cls = args
         self.__callback(joystick_cls(msg), name)
+
+    # ----------------------------------------------------------------------------------
+    #
+    # Retrieve messages
+    # ----------------------------------------------------------------------------------
 
     def msgsRecieved(self, name):
         """True when at least one named message has been received, False otherwise. """
@@ -189,6 +243,11 @@ class RosNode:
         except KeyError:
             msg = default
         return msg
+
+    # ----------------------------------------------------------------------------------
+    #
+    # Spin and shutdown node
+    # ----------------------------------------------------------------------------------
 
     def spin(self):
         """Simple wrapper for rospy.spin."""
