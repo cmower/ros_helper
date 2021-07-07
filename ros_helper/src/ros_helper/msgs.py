8ac7b4799428d0b9f76a8294bd9fe550e545293b
Chris E. Mower <chris.mower@ed.ac.uk>
2021-07-07 14:37:11 +0100

    Bugfix

--- ros_helper/src/ros_helper/msgs.py
+++ ros_helper/src/ros_helper/msgs.py
@@ -1,4 +1,4 @@
-from geometry_msgs.msg import TransformStamped, Vector3, Quaterion
+from geometry_msgs.msg import TransformStamped, Vector3, Quaternion
 from sensor_msgs.msg import JointState
 
 def transform_stamped(baseid, childid, t, p, q=[0, 0, 0, 1]):
@@ -6,7 +6,7 @@ def transform_stamped(baseid, childid, t, p, q=[0, 0, 0, 1]):
     tf.header.frame_id = baseid
     tf.header.stamp = t
     tf.transform.translation = Vector3(x=p[0], y=p[1], z=p[2])
-    tf.transform.rotation = Quaterion(x=q[0], y=q[1], z=q[2], w=q[3])
+    tf.transform.rotation = Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
     return tf
 
 def joint_state(t, n=[], p=[], v=[], e=[]):
