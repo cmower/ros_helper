cf74726918059783d5b84ddd64dc04422d1887fd
Chris E. Mower <chris.mower@ed.ac.uk>
2021-07-27 16:28:54 +0100

    Add test case for new method in config.py

--- ros_helper/test/test_config.py
+++ ros_helper/test/test_config.py
@@ -2,6 +2,7 @@
 import os
 import unittest
 
+import numpy
 import rosunit
 
 PKG = 'ros_helper'
@@ -9,6 +10,7 @@ PKG = 'ros_helper'
 # Methods to test
 from ros_helper.config import parse_filename
 from ros_helper.config import load_config
+from ros_helper.config import quaternion_from_euler_deg
 
 
 class TestConfig(unittest.TestCase):
@@ -23,6 +25,26 @@ class TestConfig(unittest.TestCase):
     def test_2(self):
         """Does load_config give the correct dictionary?"""
         self.assertEqual(load_config(self.FIND_FILENAME), self.CONFIG)
+
+    def test_3(self):
+        """Does quaternion_from_euler_deg give correct values."""
+
+        tol = 1e-7
+        test_results = []
+
+        test1_eul = numpy.zeros(3)
+        test1_quat = numpy.array([0, 0, 0, 1])
+        test_results.append(
+            numpy.linalg.norm(test1_quat - quaternion_from_euler_deg(test1_eul)) <= tol
+        )
+
+        test2_eul = numpy.array([ 57.29577951, -57.29577951,  28.64788976])
+        test2_quat = numpy.array([ 0.51174747, -0.30356422,  0.41324185,  0.6893435])
+        test_results.append(
+            numpy.linalg.norm(test2_quat - quaternion_from_euler_deg(test2_eul)) <= tol
+        )
+
+        self.assertTrue(all(test_results))
 
 
 if __name__ == '__main__':
